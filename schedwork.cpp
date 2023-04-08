d59aa4d509c072b9545fb4328c46036e9e8eafc8
Huarun Dai <huarunda@usc.edu>
2023-04-08 01:20:03 +0000

    done

--- schedwork.cpp
+++ schedwork.cpp
@@ -20,7 +20,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduleHelper(
+    AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<int>& hours,
+    int day,
+    int num
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -31,14 +39,83 @@ bool schedule(
     DailySchedule& sched
 )
 {
+	 AvailabilityMatrix temp = avail;
+   // cout<< "here" << endl;
     if(avail.size() == 0U){
         return false;
     }
+    //cout << avail.size() << endl;
     sched.clear();
     // Add your code below
-
+    vector<int> hours;
+    for(int i =0; i<avail.size(); i++){
+         hours.push_back(0);
+    }
+		sched.resize(avail.size());
+		for(int i=0; i<sched.size(); i++){
+			sched[i].resize(dailyNeed);
+		}
+    int day = avail.size();
+    // vector<vector<int>>avail_time;
+    // for(int i=0; i<avail.size();i++){
+    //     for(int j=0; j<avail[0].size; j++){
+    //         if(avail[i][j]){
+    //             avail[i].push_back(j);
+    //         }
+    //     }
+    // }
+    return scheduleHelper(temp,dailyNeed,maxShifts,sched,hours,day,0);
 
 
 
 }
 
+// bool isValid(vector<size_t>& hours,const size_t maxShifts){
+//     for(vector<size_t> iterator it= hours.begin(); it!=hours.end(); ++it){
+//         if(*it>=maxShifts) return false;
+//     }
+//     return true;
+// }
+
+bool scheduleHelper(
+    AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<int>& hours,
+    int day,
+    int num
+) {
+    //cout<< "here" << endl;
+    if(day==0){
+        return true;
+    }
+    else if(num == dailyNeed){
+        return scheduleHelper(avail,dailyNeed,maxShifts,sched,hours,day-1,0);
+    }
+		else{
+			for(int i = 0; i<avail[0].size(); i++){
+				//cout<< "here" << endl;
+					if(avail[avail.size()-day][i]){
+						//cout<< "here" << avail.size()-day << " " << sched.size() << endl;
+							sched[avail.size()-day][num]=i;
+							//cout<< "here" << endl;
+							hours[i] += 1;
+							avail[avail.size()-day][i] = 0;
+							if(hours[i]<=maxShifts){
+								  //cout<< "here" << endl;
+									bool result = scheduleHelper(avail,dailyNeed,maxShifts,sched,hours,day,num+1);
+									if(result){
+											return true;
+									}
+							}
+						hours[i] -= 1;
+						sched[avail.size()-day][num]=INVALID_ID;
+						avail[avail.size()-day][i] = 1;
+					}
+					//avail[avail.size()-day][i] = 1;       
+			}
+		}
+
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,8 +13,19 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void fwordle(
+		std::string& in,
+    std::multiset<char>& floating,
+		const std::set<std::string>& dict,
+		std::set<std::string>& suggestion,
+		int size,
+		int r_size
+		);
 
-
+bool check(
+	  const std::set<std::string>& dict,
+		string& comb
+		);
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
@@ -22,7 +33,83 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+		std::set<std::string>suggestion ;
+		int size = in.size();
+		int r_size = 0;
+		std::multiset<char> floating_;
+		for(int i=0; i<in.size(); i++){
+			if(in[i]=='-'){
+				r_size++;
+			}
+		}
+		for(int i=0; i<floating.size(); i++){
+			floating_.insert(floating[i]);
+		}		
+		string temp = in;
+		//cout << r_size << endl;
+		fwordle(temp, floating_, dict, suggestion, size, r_size);
+		return suggestion;
 }
 
 // Define any helper functions here
+void fwordle(
+		std::string& in,
+    std::multiset<char>& floating,
+		const std::set<std::string>& dict,
+		std::set<std::string>& suggestion,
+		int size,
+		int r_size
+		)
+{
+	//cout << in << " " << size << " " << r_size << " " << floating.size()<< endl;
+	if(r_size<floating.size()){
+		//cout << "here" << endl;
+		return;
+	}
+	if(size==0){
+		if(check(dict,in)){
+			suggestion.insert(in);
+			return;
+		}
+		return;
+	}
+	if(in[in.size()-size]=='-'){
+   
+		for(int i=97; i<123; i++){
+			char temp = i;
+			//cout<<temp;
+			in[in.size()-size]= temp;
+			if(floating.find(temp)!= floating.end()){
+				floating.erase(floating.find(temp));
+				fwordle(in,floating,dict,suggestion,size-1,r_size-1);
+				in[in.size()-size]='-';
+				floating.insert(temp);
+			}
+			else{
+				fwordle(in,floating,dict,suggestion,size-1,r_size-1);
+				 //cout << size << endl;
+				in[in.size()-size]='-';
+				//cout << in << endl;
+			}
+		}
+		return;
+	}
+	else{
+		fwordle(in,floating,dict,suggestion,size-1,r_size);
+		return;
+	}
+
+
+}
+
+bool check(
+	  const std::set<std::string>& dict,
+		string& comb
+		)
+{
+	if(dict.find(comb)!=dict.end()){
+		return true;
+	}
+	return false;
+
+}
